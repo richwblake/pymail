cdba5e25599ecb1d8d2ced4b2594601ecdcab63a
Wills Blake <richwblake@gmail.com>
2024-03-06 13:26:25 -0500

    Adds seed file for testing. Adds get and post routes for messages

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from pprint import pprint
@@ -25,7 +25,22 @@ def meta():
 def messages():
     if request.method == 'GET':
         messages = Message.query.all()
-        return make_response(messages, 200)
+
+        message_dicts = [m.to_dict() for m in messages]
+
+        return make_response(message_dicts, 200)
+
+    elif request.method == 'POST':
+        message = Message()
+
+        for attr in request.get_json():
+            setattr(message, attr, request.get_json()[attr])
+
+        db.session.add(message)
+        db.session.commit()
+        
+        return make_response(message.to_dict(), 201)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
