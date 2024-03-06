619ba4757e381f14e9bec61722af1280fae70d54
Wills Blake <richwblake@gmail.com>
2024-03-06 10:28:21 -0500

    Adds metadata route to debug request headers

--- server/app.py
+++ server/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response
+from flask_cors import CORS
 from flask_migrate import Migrate
+from pprint import pprint
+import ipdb
 
 from models import db
 
@@ -8,8 +11,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+CORS(app)
 migrate = Migrate(app, db)
+
 db.init_app(app)
+
+@app.route('/meta')
+def meta():
+    pprint(dict(request.headers))
+    return make_response({'message': 'request okay, metadata logged'}, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
