857043b0ee0539a6b6bf62a9afecb98b35f2b4d4
Wills Blake <richwblake@gmail.com>
2024-03-07 11:33:16 -0500

    Adds CORS restriction for domain origin

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-CORS(app)
+CORS(app, resources={r'/*': {'origins': ['https://willsblake.tech', 'https://www.willsblake.tech']}})
 migrate = Migrate(app, db)
 
 db.init_app(app)
