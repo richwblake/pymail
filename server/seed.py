f1e5861cbee72e0848d2f6e2a1ddd0e15d2f3894
Wills Blake <richwblake@gmail.com>
2024-03-13 11:06:37 -0400

    Seed file correctly builds relationships, schema updated

--- server/seed.py
+++ server/seed.py
@@ -1,6 +1,6 @@
 from faker import Faker
 from app import app
-from models import db, Message
+from models import db, Receiver, Message, MessageField
 
 fake = Faker()
 
@@ -12,16 +12,22 @@ def create_messages():
 
     messages = []
 
-    for i in range(5):
-        m = Message(
-                name = fake.name(),
-                email = fake.email(),
-                content = fake.sentence(),
-                )
+    r1 = Receiver(name=fake.name(), email=fake.email())
+    r2 = Receiver(name=fake.name(), email=fake.email())
+    r3 = Receiver(name=fake.name(), email=fake.email())
 
-        messages.append(m)
+    db.session.add(r1)
+    db.session.add(r2)
+    db.session.add(r3)
 
-    db.session.add_all(messages)
+    r1.messages = [Message() for i in range(0, 5)]
+    r2.messages = [Message() for i in range(0, 5)]
+    r3.messages = [Message() for i in range(0, 5)]
+
+    for receiver in Receiver.query.all():
+        for message in receiver.messages:
+            message.message_fields = [MessageField(title=fake.country(), content=fake.sentence()) for i in range(0, 3)]
+
     db.session.commit()
 
 if __name__ == '__main__':
